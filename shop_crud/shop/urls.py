1b960b695eda6c357dbe6435798dc8b0696808f3
BwanaQ <thunjawax@gmail.com>
2021-12-02 16:49:06 +0300

    ft-Product-CRUD

--- shop_crud/shop/urls.py
+++ shop_crud/shop/urls.py
@@ -1,13 +1,13 @@
 from django.urls import path
 from .views import *
 urlpatterns = [
-    path('category/', CategoryIndexView.as_view(), name="category-home"),
+    path('categories/', CategoryIndexView.as_view(), name="category-home"),
     path('new/', CategoryCreateView.as_view(),name='category-create-view'),
     path('category/<slug:slug>/', CategoryDetailedView.as_view(), name="category-detail-view"),
     path('category/<slug:slug>/update/', CategoryUpdateView.as_view(), name="category-update-view"),
     path('category/<slug:slug>/delete/', CategoryDeleteView.as_view(), name="category-delete-view"),
 
-    path('product/', ProductListView.as_view(), name="product-home"),
+    path('products/', ProductListView.as_view(), name="product-home"),
     path('product/<slug:slug>/', ProductDetailView.as_view(), name="product-detail-view"),
     path('product-new/', ProductCreateView.as_view(),name='product-create-view'),
     path('product/<slug:slug>/update/', ProductUpdateView.as_view(), name="product-update-view"),
